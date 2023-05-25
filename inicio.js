e827ac89de79fa616b2a59c1152d4cc73856b691
UlisesCasal <129341282+CasalGabriel@users.noreply.github.com>
2023-05-25 04:13:05 -0300

    Subo api arreglada
    Queda hacer funcionar que el contenido se muestre por pantalla

--- inicio.js
+++ inicio.js
@@ -1,51 +1,57 @@
 window.addEventListener('load', getDatos);
 
-function limpiarFecha(fecha){
+function limpiarFecha(fecha) {
     //Metodo auxiliar que permite obtener la fecha parseando el string
-    return fecha.substring(0,10);
+    return fecha.substring(0, 10);
 }
 
-function limpiarDatos(texto){
+function limpiarDatos(texto) {
     //Metodo que a partir de los datos recibidos, obtiene, el nombre de las fechas patrias y la fecha
-    let nombresFechas = []
-    let fechas = []
+    let listaTitulos = []
+    let listaDiario = []
 
     i = 0;
-    salida = "";
-    while(i < texto.length){
-        fecha = limpiarFecha(texto[i].date);
-        nombre = texto[i].name;
-        nombresFechas.push(nombre);
-        fechas.push(fecha);
-        i ++;
+    var largo = Object.keys(texto).length;
+    
+    while (i < 10) {
+        articulo = texto.articles[i];
+        diario = articulo.author;
+        titulo = articulo.title;
+        listaTitulos.push(titulo);
+        listaDiario.push(diario);
+        i++;
     }
 
-    return nombresFechas, fechas;
+    return [listaTitulos, listaDiario];
 }
 
-function mostrarContenido(nombresFechas, fechas){
+function mostrarContenido(diarios, titulos) {
     // metodo que muestra el contenido obtenido de la API
     let i = 0;
     let elementoContenedor;
-    while (i < nombresFechas.length){
-        elementoContenedor = document.createElement('div');
-        elementoContenedor.textContent = JSON.stringify("⚪ " + fechas[i] + " " + nombresFechas[i]);
+    while (i < titulos.length) {
+        elementoContenedor = document.createElement('a');
+        diarioElemento = diarios[1];
+        tituloElemento = titulos[i];
+
+        elementoContenedor.textContent;
         document.body.appendChild(elementoContenedor);
-        i ++;
+        i++;
     }
 }
 
-function getDatos(){
-    let nombresFechas, fechas = [];
+async function getDatos() {
+    var url = 'https://newsapi.org/v2/top-headlines?' + 
+    'country=ar&' +
+    'apiKey=19bf50de65cb461bb2e89e7f5b4ef347';
 
-    fetch('https://api.generadordni.es/v2/holidays/holidays?country=AR&year=2023')
-        .then(response => response.json())
-        .then(data => {
-            nombresFechas, fechas = limpiarDatos(data);
-            mostrarContenido(nombresFechas, fechas);
-            
-        
-        
-  })
-  .catch(error => console.error(error));
+    var req = new Request(url);
+
+    let response = await fetch(req);
+    let data = await response.json();
+    // Aquí puedes guardar el objeto JSON en una variable
+    let miVariable = data;
+    console.log(miVariable.articles[0].author);
+    let [titulos, diarios] = limpiarDatos(miVariable);
+    mostrarContenido(titulos, diarios);
 }
