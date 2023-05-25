ea8cfa583d7d773d107204f22153e689bc5d428e
UlisesCasal <129341282+CasalGabriel@users.noreply.github.com>
2023-05-24 22:27:41 -0300

    Update inicio.js

--- inicio.js
+++ inicio.js
@@ -1,31 +1,49 @@
 window.addEventListener('load', getDatos);
-function parsearFecha(fecha){
-    fecha = fecha.substring(0,10)
-    return fecha
-}
 
 function limpiarFecha(fecha){
+    //Metodo auxiliar que permite obtener la fecha parseando el string
     return fecha.substring(0,10);
 }
 
 function limpiarDatos(texto){
+    //Metodo que a partir de los datos recibidos, obtiene, el nombre de las fechas patrias y la fecha
+    let nombresFechas = []
+    let fechas = []
+
     i = 0;
     salida = "";
     while(i < texto.length){
         fecha = limpiarFecha(texto[i].date);
         nombre = texto[i].name;
-        salida += fecha + nombre + "/n";
+        nombresFechas.push(nombre);
+        fechas.push(fecha);
+        i ++;
     }
 
-    return salida;
+    return nombresFechas, fechas;
+}
+
+function mostrarContenido(nombresFechas, fechas){
+    // metodo que muestra el contenido obtenido de la API
+    let i = 0;
+    let elementoContenedor;
+    while (i < nombresFechas.length){
+        elementoContenedor = document.createElement('div');
+        elementoContenedor.textContent = JSON.stringify("⚪ " + fechas[i] + " " + nombresFechas[i]);
+        document.body.appendChild(elementoContenedor);
+        i ++;
+    }
 }
 
 function getDatos(){
+    let nombresFechas, fechas = [];
+
     fetch('https://api.generadordni.es/v2/holidays/holidays?country=AR&year=2023')
         .then(response => response.json())
         .then(data => {
-            datos = limpiarDatos(data);
-            console.log(datos);   
+            nombresFechas, fechas = limpiarDatos(data);
+            mostrarContenido(nombresFechas, fechas);
+            
         
         
   })
